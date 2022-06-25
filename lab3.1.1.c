fa04e0a38a3c75c477041e440ce38436ef7d806e
mifahmiali <2021498082@student.uitm.edu.my>
2022-06-26 03:00:38 +0800

    fork3.1.1

---
+++ lab3.1.1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+void sigint_handler(int sig);
+void sigtstp_handler(int sig);
+void sigquit_handler(int sig);
+
+int main(void)
+{
+	char s[200];
+
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
+	{
+		perror("signal");
+		exit(1);
+	}
+
+        if (signal(SIGTSTP, sigtstp_handler) == SIG_ERR)
+        {
+                perror("signal");
+                exit(1);
+        }
+
+	if (signal(SIGQUIT, sigquit_handler) == SIG_ERR)
+        {
+                perror("signal");
+                exit(1);
+        }
+
+
+	printf("Enter a string:\n");
+
+	if (fgets(s, 200, stdin) == NULL)
+		perror("gets");
+	else
+		printf("You entered: %s\n", s);
+
+	return (0);
+
+}
